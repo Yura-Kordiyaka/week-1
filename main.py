b9be4af8cc3b7987a925014f78b2793f6b9d6bed
Yura <96499300+yurarasik@users.noreply.github.com>
2022-07-09 12:43:41 +0300

    second commit

--- main.py
+++ main.py
@@ -1,26 +1,8 @@
-# x = int(input("input your first number ="))
-# y = int(input("input second number = "))
-# print("your answer ", x + y)
-#
+def result(my_list):
+    for j in range(0, len(my_list)):
+        if my_list[j] < 5:
+            print(my_list[j])
 
-name = str(input("input your data : "))
-for j in range(0, len(name)):
-    print(name[j] * 2,end='')
-# a = []
-# k = 0
-# while k < 4:
-#     a.append(int(input("input your number : ")))
-#     k = k + 1
-# b = a[0]
-# c = a[0]
-# for x in range(1, len(a)):
-#     if b < a[x]:
-#         b = a[x]
-#     elif c > a[x]:
-#         c = a[x]
-#
-# print("max = ", b, "min = ", c)
-# print(max(a))
-# print(min(a))
-f = str(input("input your data : "))
-print(f[::-1])
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+result(a)
